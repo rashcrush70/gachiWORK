a723172260b18173d21a9c33e84f3a2364df0617
Markokakola <148535019+markokakola@users.noreply.github.com>
2023-11-24 11:55:38 +0500

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,5 @@
-
+// JavaScript for adding products to the cart
+function addToCart(productId) {
+    alert('Product ' + productId + ' added to cart!');
+    // Add logic to handle the cart (e.g., updating a shopping cart object)
+}
